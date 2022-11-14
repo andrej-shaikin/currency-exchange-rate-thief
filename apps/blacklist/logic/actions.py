3285bbcc94e46039deb427ac9725653cf379e862
Andrey Shaikin <kiwibon@yandex.ru>
2022-11-14 18:48:12 +0300

    получение и обновление сведений о black list из ЦБ

--- apps/blacklist/logic/actions.py
+++ apps/blacklist/logic/actions.py
@@ -9,6 +9,7 @@ from lxml import etree
 from starlette import status
 
 from apps.blacklist.dto import BlackListItemDto
+from apps.blacklist.models import BlackList, BlackListSign
 
 logger = getLogger(__name__)
 
@@ -35,3 +36,20 @@ async def get_blacklist_list_from_cbr() -> Optional[list[BlackListItemDto]]:
                 )
             )
         return black_list
+
+
+async def update_black_list() -> None:
+    # TODO обновлять объекты, обойтись без удаления
+    BlackList.objects.delete()
+
+    for black_list_item in await get_blacklist_list_from_cbr():
+        black_list_sign, _ = await BlackListSign.objects.update_or_create(name=black_list_item.name)
+        await BlackList.objects.create(
+            dt=black_list_item.dt,
+            inn=black_list_item.inn,
+            closed=black_list_item.closed,
+            address=black_list_item.address,
+            sites=','.join(black_list_item.sites),
+            sign=black_list_sign,
+            name=black_list_item.name,
+        )
--- apps/blacklist/tasks.py
+++ apps/blacklist/tasks.py
@@ -1,19 +1,7 @@
-from apps.blacklist.logic.actions import get_blacklist_list_from_cbr
-from apps.blacklist.models import BlackList, BlackListSign
+import asyncio
+
+from apps.blacklist.logic.actions import update_black_list
 
 
-async def update_black_list_task() -> None:
-    # TODO обновлять объекты, обойтись без удаления
-    await BlackList.objects.all().delete()
-
-    for black_list_item in await get_blacklist_list_from_cbr():
-        black_list_sign, _ = BlackListSign.objects.update_or_create(name=black_list_item.name)
-        BlackList.objects.create(
-            dt=black_list_item.dt,
-            inn=black_list_item.inn,
-            closed=black_list_item.closed,
-            address=black_list_item.address,
-            sites=','.join(black_list_item.sites),
-            sign=black_list_sign,
-            name=black_list_item.name,
-        )
+def update_black_list_task() -> None:
+    asyncio.run(update_black_list())
